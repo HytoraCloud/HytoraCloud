aeb0a4581baa489ab49a5cba31b683919e293a43
Lystx <63949927+Lystx@users.noreply.github.com>
2021-07-30 11:28:35 +0200

    Update CloudFlareTest.java

--- hytoracloud-driver/src/test/java/CloudFlareTest.java
+++ hytoracloud-driver/src/test/java/CloudFlareTest.java
@@ -7,8 +7,7 @@ public class CloudFlareTest {
 
     public static void main(String[] args) {
 
-        new CloudFlareAPI(new CloudFlareAuth("f9e99a4817d03f44ae42d08f39ffb4b0753d3", "wtfcxt@gmail.com"), "5dab5b08e0a4c182ba774b8cd2915ea0");
-
+        
 
     }
 }
